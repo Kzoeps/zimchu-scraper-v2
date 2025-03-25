0a2bbf41b6b5c35c2152fd89002288a75c8cbb18
kzoeps <kzoepa@gmail.com>
2025-03-25 19:00:48 -0400

    feat: make it run headless when done through shell script

--- run_main.sh
+++ run_main.sh
@@ -4,6 +4,7 @@ cd /home/kzoeps/Projects/zimchu/zimchu-scraper-v2
 
 echo "=== Main script started at $(date) ===" >> main_errors.log
 source venv/bin/activate
+export RUN_HEADLESS=True
 python main.py
 
 echo "=== Main script completed at $(date) ===" >> main_errors.log
