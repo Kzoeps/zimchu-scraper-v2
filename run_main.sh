876385251fa83858c89bad87377909ee24f25c35
kzoeps <kzoepa@gmail.com>
2025-03-27 23:14:21 -0400

    feat: run clean up after scraping

--- run_main.sh
+++ run_main.sh
@@ -6,6 +6,6 @@ echo "=== Main script started at $(date) ===" >> main_errors.log
 source venv/bin/activate
 export RUN_HEADLESS=True
 python main.py
-
+python cleanup_listings.py
 echo "=== Main script completed at $(date) ===" >> main_errors.log
 deactivate
